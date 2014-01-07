35d0afc0a256bcb58b2d8fa6231282b35719a812
Nick Lima <nlima@mit.edu>
2014-01-06 19:02:01 -0500

    Made robot player
    made stuff for robot player should be ready to edit

--- OperationScreamingFist/RobotPlayer.java
+++ OperationScreamingFist/RobotPlayer.java
@@ -1,4 +1,11 @@
+package OperationScreamingFist;
 
+import battlecode.common.Direction;
+import battlecode.common.GameConstants;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+import battlecode.common.*;
+import java.util.*;
 
 public class RobotPlayer 
 {
