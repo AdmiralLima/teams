c6a270d14d0390f8c3dcf3584bc9c3530e716eef
Thomas Alcorn <alcorn@mit.edu>
2014-01-13 17:27:14 -0500

    merging

--- OperationScreamingFist/actions/Intercept.java
+++ OperationScreamingFist/actions/Intercept.java
@@ -23,57 +23,46 @@ public class Intercept {
         rc.yield();
         
         if (rc.isActive()) {
-        Robot[] nearbyEnemies = rc.senseNearbyGameObjects(Robot.class,
-                rc.getType().sensorRadiusSquared,
-                rc.getTeam().opponent());
-        for (Robot test : nearbyEnemies) {
-            if (enemy.equals(test)) {System.out.println("enemy is nearby");}
-        }
-        if (rc.canSenseObject(enemy)) {
-            info = rc.senseRobotInfo(enemy);
-            MapLocation enemyLoc = info.location;
-            System.out.println("enemy location is " + enemyLoc.toString());
-        }
-        
-        MapLocation enemyLoc = enemyLocPrev;
-        Direction enemyTraj = myLoc.directionTo(rc.senseEnemyHQLocation());
-        
-        while (info.health > 0 && rc.canSenseObject(enemy)) {
-            if (rc.isActive()) {
-            if (rc.canSenseObject(enemy)) {
-            // get enemy robot info
-            info = rc.senseRobotInfo(enemy);
-            enemyLoc = info.location;
-            enemyTraj = enemyLocPrev.directionTo(enemyLoc);
-            //System.out.println("Spotted enemy at " + enemyLoc.toString() + " facing " + enemyTraj.toString());
-            if (rc.canAttackSquare(enemyLoc)) {
-                Attacks.attackLocation(enemyLoc);
-            }
-            } else { 
-            // get my loc
-            myLoc = rc.getLocation();
-            // if the enemy hasn't moved, head straight for them
-            if (enemyTraj.equals(Direction.OMNI) || enemyTraj.equals(Direction.NONE)) {
-                System.out.println("Following " + info.type.toString() + " with health " + info.health);
-                System.out.println(myLoc.directionTo(enemyLoc).toString());
-                Soldier.move(myLoc.directionTo(enemyLoc));
-            } else { 
-                // else predict enemies future position at future time
-                MapLocation goal = enemyLoc;
-                for (int i = timeToEnemy; i > 0; i--) {
-                    goal.add(enemyTraj);
+
+            MapLocation enemyLoc = enemyLocPrev;
+            Direction enemyTraj = myLoc.directionTo(rc.senseEnemyHQLocation());
+
+            while (info.health > 0 && rc.canSenseObject(enemy)) {
+                if (rc.isActive()) {
+                    if (rc.canSenseObject(enemy)) {
+                        // get enemy robot info
+                        info = rc.senseRobotInfo(enemy);
+                        enemyLoc = info.location;
+                        enemyTraj = enemyLocPrev.directionTo(enemyLoc);
+                        //System.out.println("Spotted enemy at " + enemyLoc.toString() + " facing " + enemyTraj.toString());
+                        if (rc.canAttackSquare(enemyLoc)) {
+                            Attacks.attackLocation(enemyLoc);
+                        }
+                    } else { 
+                        // get my loc
+                        myLoc = rc.getLocation();
+                        // if the enemy hasn't moved, head straight for them
+                        if (enemyTraj.equals(Direction.OMNI) || enemyTraj.equals(Direction.NONE)) {
+                            System.out.println("Following " + info.type.toString() + " with health " + info.health);
+                            System.out.println(myLoc.directionTo(enemyLoc).toString());
+                            Soldier.move(myLoc.directionTo(enemyLoc));
+                        } else { 
+                            // else predict enemies future position at future time
+                            MapLocation goal = enemyLoc;
+                            for (int i = timeToEnemy; i > 0; i--) {
+                                goal.add(enemyTraj);
+                            }
+                            // get direction to future position
+                            Direction moveTo = goal.directionTo(myLoc);
+                            // move toward it
+                            System.out.println("Moving toward mobile enemy in direction " + moveTo);
+                            Soldier.move(moveTo);
+                        }
+                    }
+                    enemyLocPrev = enemyLoc;
+                    rc.yield();
                 }
-                // get direction to future position
-                Direction moveTo = goal.directionTo(myLoc);
-                // move toward it
-                System.out.println("Moving toward mobile enemy in direction " + moveTo);
-                Soldier.move(moveTo);
             }
-            }
-            enemyLocPrev = enemyLoc;
-            rc.yield();
-            }
-        }
         }
     }
     
