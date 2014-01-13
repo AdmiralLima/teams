647c2e45dad55c2dce515eef4cb06800b784128e
Thomas Alcorn <alcorn@mit.edu>
2014-01-13 18:37:33 -0500

    merging

--- T_800/Strategy/Turtle.java
+++ T_800/Strategy/Turtle.java
@@ -20,9 +20,9 @@ public class Turtle implements Strategy {
         // TODO: set new orders for team
             // if there is no pastr and/or noisetower near hq, get a soldier to build one
                 // check if there is a noisetower within squareradius of 1
-                    // if not, tell next soldier built to make one
+                    // if not, tell next soldier within sqrad=1 to construct noistwr
                     // if so, check if there is a pastr within squareradius of 1
-                        // if not, tell next soldier built to make one
+                        // if not, tell next soldier within sqrad=1 to construct pastr
             // else, set new goal location for swarm
 
     }
@@ -30,6 +30,10 @@ public class Turtle implements Strategy {
     @Override
     public void runSOLDIER() {
         // read broadcasted orders from HQ
+        // if within 1 square of HQ
+            // check if hq needs noistwr or pastr
+                // if so, construct
+        // else go to goal location
 
     }
 
