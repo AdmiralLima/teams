8ce38008f715f45aed22d1a4cf29aef1d4bfb97d
Nicholas DeVane Lima <Nick@Nicholass-MacBook-Pro.local>
2014-01-13 21:39:36 -0500

    nothing really

--- T_800/Complex/Spawn.java
+++ T_800/Complex/Spawn.java
@@ -1,9 +1,32 @@
 package T_800.Complex;
-//TODO: everything.
+
+import battlecode.common.*;
+import T_800.*;
+
+/**
+ * Contains well thought out methods for spawning units from an HQ.
+ */
 public class Spawn 
 {
-	public static boolean spawn()
+	private static RobotController rc = RobotPlayer.rc;
+	
+	/**
+	 * Spawns a unit in any available direction, starting with the direction of the enemy HQ.
+	 * 
+	 * @return boolean - Returns true if a successful spawn in executed, false otherwise.
+	 * @throws GameActionException
+	 */
+	public static boolean spawn() throws GameActionException
 	{
+		Direction desired = rc.getLocation().directionTo(rc.senseEnemyHQLocation());
+		for (int i = 0; i < 7; i++)
+		{
+			if (T_800.Basic.Spawn.spawnDirection(desired))
+			{
+				return true;
+			}
+			desired = desired.rotateRight();
+		}
 		return false;
 	}
 }
