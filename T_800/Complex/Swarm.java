01a15c9f0f4ebe4814391376c30c8de53a2690e7
Nicholas DeVane Lima <Nick@Nicholass-MacBook-Pro.local>
2014-01-13 22:00:07 -0500

    stored skynet

--- T_800/Complex/Swarm.java
+++ T_800/Complex/Swarm.java
@@ -1,5 +1,44 @@
 package T_800.Complex;
 
-public class Swarm {
+import T_800.*;
+import battlecode.common.*;
 
+/**
+ * Contains methods for simple swarming techniques.
+ */
+public class Swarm 
+{
+	private static RobotController rc = RobotPlayer.rc;
+	
+	public static boolean swarm(Robot[] friends) throws GameActionException
+	{
+		MapLocation[] friendLocation = new MapLocation[friends.length];
+		for (int i = 0; 0 < friends.length; i++)
+		{
+			if (rc.canSenseObject(friends[i]))
+			{
+				friendLocation[i] = rc.senseRobotInfo(friends[i]).location;
+			}
+		}
+		int xTotal = 0;
+		int yTotal = 0;
+		int counter = 0;
+		for (MapLocation loc : friendLocation)
+		{
+			if (loc != null)
+			{
+				xTotal += loc.x;
+				yTotal += loc.y;
+				counter ++;
+			}
+		}
+		if (counter == 0)
+		{
+			return false;
+		}
+		MapLocation desiredLoc = new MapLocation(xTotal/counter, yTotal/counter);
+		Direction desiredDir = rc.getLocation().directionTo(desiredLoc);
+		
+		return false;
+	}
 }
